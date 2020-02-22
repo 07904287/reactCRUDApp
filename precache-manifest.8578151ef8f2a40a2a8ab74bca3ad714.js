ce15c7348c0c7d89668565c0c77c12ed84ad9e16
unknown <nagy.richard.bp@gmail.com>
2020-02-22 20:02:47 +0000

    Updates

---
+++ precache-manifest.8578151ef8f2a40a2a8ab74bca3ad714.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59c3ee107c3379ab8772ecdf30327b49",
+    "url": "/ricardobayes.github.io/reactCRUDApp/index.html"
+  },
+  {
+    "revision": "31af288407280fa00fbb",
+    "url": "/ricardobayes.github.io/reactCRUDApp/static/css/main.80a33dc6.chunk.css"
+  },
+  {
+    "revision": "eea8ac979d827c87b940",
+    "url": "/ricardobayes.github.io/reactCRUDApp/static/js/2.85a6ac40.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/ricardobayes.github.io/reactCRUDApp/static/js/2.85a6ac40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31af288407280fa00fbb",
+    "url": "/ricardobayes.github.io/reactCRUDApp/static/js/main.4f2e8a57.chunk.js"
+  },
+  {
+    "revision": "c5bfe5145b1153e9ea00",
+    "url": "/ricardobayes.github.io/reactCRUDApp/static/js/runtime-main.bbef560a.js"
+  }
+]);
